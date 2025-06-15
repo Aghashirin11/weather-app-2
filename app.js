af1ed8d6eada78a1edd1b06ac0efaadd1f4de5ed
Aghashirin Babayev <145477678+Aghashirin11@users.noreply.github.com>
2025-06-15 22:48:32 +0400

    Update app.js

--- app.js
+++ app.js
@@ -45,6 +45,83 @@ btnEl.addEventListener('click',(e) =>{
 } )
 
 
+____________________________________________________________________________________________--
+
+const dayEl = document.querySelector('.default_day');
+const dateEl = document.querySelector('.default_date');
+const btnEl = document.querySelector('.btn_search')
+const searchBar = document.querySelector('.search_bar');
+
+const appContainer = document.querySelector('.icons')
+
+const description = document.querySelector('.icons img')
+
+const apiKey = '173623ae52e316a5245c191d628e93ab'
+
+const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+]
+
+
+const day = new Date();
+const dayName = days[day.getDay()]
+dayEl.textContent = dayName
+
+
+let month = day.toLocaleString('en', { month: "long" });
+let date = day.getDate();
+let year = day.getFullYear();
+
+dateEl.textContent = month + " " + date + " " + year
+
+
+btnEl.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if (searchBar.value !== "") {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchBar.value}&appid=${apiKey}&units=metric`)
+            .then(response => response.json())
+            .then(json => {
+                if (json.cod === '404') {
+                    appContainer.style.display = 'none'
+                }
+                else {
+                    console.log(json);
+
+                }
+            }
+
+            )
+
+    }
+    else {
+        return;
+
+    }
+
+
+})
+
+
+
+function get(){fetch(`https://api.openweathermap.org/data/2.5/forecast?q=baku&appid=173623ae52e316a5245c191d628e93ab&units=metric`)
+.then(response => response.json())
+.then(json => {
+    console.log(json);
+    
+})}
+            
+
+
+get()
+
+
 
 
 
